9da672ed2db41dbf6d39afa4a2c8630f3b704cb5
Mauricio Ramirez <mau_rr01@hotmail.com>
2024-05-17 20:59:29 -0600

    Se conecta a la base de datos de usuarios

--- login_ec2/login-middleware.php
+++ login_ec2/login-middleware.php
@@ -1,12 +1,19 @@
 <?php 
-    $email = "test@uach.mx";
-    $password="abcd1234";
+   
+    $email = $_POST['email'];
+    $password=$_POST['password'];
 
-    /*
-    * AQUÍ HACER LA CONEXIÓN A LA BASE DE DATOS PARA VERIFICAR SÍ ESTÁ TODO BIEN
-    */
+    $conexion = mysqli_connect("localhost","root","","final_db");
+    $query = "select * from users where email = '".$email."'; ";
+    $resultado = mysqli_query($conexion,$query);
+    $registro = mysqli_fetch_array($resultado);
+    
 
-    if($email == $_POST["email"] && $password == $_POST["password"]){
+    $correct_email = $registro["email"];
+    $correct_password = $registro["password"];
+    $user_name = $registro["user_name"];
+
+    if($correct_email == $email && $correct_password == $password){
         //ENVÍA A BIENVENIDA
         //setear cookie
         $cookie_expire_date = time() + 60;
@@ -14,8 +21,8 @@
         /*
         no va  a ser suficiente un cookie usuario, se va a necesitar un mpetodo de autenticación
         que el valor guardado en el cookie usuario valide la sesión, por ejemplo un jwt.
-        */
-        setcookie("usuario","USUARIO PRUEBA", $cookie_expire_date);
+       */
+        setcookie("usuario",$user, $cookie_expire_date);
         header("Location: welcome.php");
     }
     else{
@@ -24,6 +31,6 @@
         //MOSTRAR MENSAJE DE QUE ESTÁ MAL
         $cookie_expire_date = time() + 5;
         setcookie("credentials_error","true", $cookie_expire_date);
-    }
+    } 
 
 ?>
